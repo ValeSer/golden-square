a17167b65b841d856c5b0025f499bed2560dabc3
Valeria Serci <balerty84@gmail.com>
2023-02-14 16:09:34 +0000

    add test to raise error in case of string passed

--- spec/counter_spec.rb
+++ spec/counter_spec.rb
@@ -5,4 +5,15 @@ RSpec.describe Counter do
     counter = Counter.new 
     expect(counter.report).to eq 'Counted to 0 so far'
   end
+
+  it 'returns 5 if 5 is added' do
+    counter = Counter.new
+    counter.add(5)
+    expect(counter.report).to eq "Counted to 5 so far"
+  end
+
+  it 'returns error if not a number is passed' do
+    counter = Counter.new
+    expect {counter.add('hello')}.to raise_error(TypeError)
+  end
 end
