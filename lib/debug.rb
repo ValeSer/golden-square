85d5726504e83e353bd02417b9e75017e3749c03
Valeria Serci <balerty84@gmail.com>
2023-02-15 12:22:40 +0000

    debugged

--- lib/debug.rb
+++ lib/debug.rb
@@ -1,18 +1,16 @@
 def find_missing(sequence)
   consecutive     = sequence.each_cons(2)
-  differences     = consecutive.map { |a,b| b - a }
+ 
+  differences     = consecutive.map { |a,b| b + a }
+
+ 
   sequence        = differences.max_by { |n| differences.count(n) }
-  missing_between = consecutive.find { |a,b| (b + a) != sequence }
-  missing_between.first + sequence
+  
+  missing_between = consecutive.find { |a,b| (b - a) != sequence }
+
+  missing_between.first + sequence 
 end
 
-find_missing([2,4,6,10]) # should be 8
-find_missing([3,6,9,15]) # should be 12
-find_missing([1,2,3,5]) # should be 4
-
-
-
-
-
-
-
+puts find_missing([2,4,6,10]) # should be 8
+puts find_missing([3,6,9,15]) # should be 12
+puts find_missing([1,2,3,5]) # should be 4
