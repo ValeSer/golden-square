e63120cbc06a8e68e0cfe2366fb6ad3e5bd6be3e
Valeria Serci <balerty84@gmail.com>
2023-02-15 10:51:21 +0000

    fix typos

--- spec/gratitudes_spec.rb
+++ spec/gratitudes_spec.rb
@@ -1,18 +1,18 @@
 require 'gratitudes'
 
 RSpec.describe Gratitudes do
-  it 'returnes "Be grateful for: " initially' do
+  it 'returns "Be grateful for: " initially' do
     gratitude = Gratitudes.new
     expect(gratitude.format).to eq "Be grateful for: "
   end
 
-  it 'returnes "Be grateful for: life"' do
+  it 'returns "Be grateful for: life"' do
     gratitude = Gratitudes.new
     gratitude.add('life')
     expect(gratitude.format).to eq "Be grateful for: life"
   end
 
-  it 'returnes "Be grateful for: life, health"' do
+  it 'returns "Be grateful for: life, health"' do
     gratitude = Gratitudes.new
     gratitude.add('life')
     gratitude.add('health')
