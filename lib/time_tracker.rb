25c1b21cab685add713e854610c8c75b9aaa7a84
Valeria Serci <balerty84@gmail.com>
2023-02-15 18:55:33 +0000

    implement method and fix test

--- lib/time_tracker.rb
+++ lib/time_tracker.rb
@@ -1,5 +1,11 @@
 
 def time_tracker(text)
-  text = ''
-  estimated_time = "0 minutes and 0 seconds"
+  words = text.split(' ').length
+  if words == 0
+    return "0 minutes and 0 seconds"
+  else
+    minutes = words / 200
+    seconds = (words % 200)* 60 / 200
+    return "#{minutes} minutes and #{seconds} seconds"
+  end
 end
--- spec/time_tracker_spec.rb
+++ spec/time_tracker_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'time_tracker' do
     expect(time_tracker('')).to eq '0 minutes and 0 seconds'
   end
 
-  #it 'returns "0 minutes and 1 second" when 5 words' do
-   # expect(time_tracker('A sentence with five words')).to eq '0 minutes and 1 second'
-  #end
+  it 'returns "0 minutes and 1 second" when 5 words' do
+    expect(time_tracker('A sentence with five words')).to eq '0 minutes and 1 seconds'
+  end
 end
