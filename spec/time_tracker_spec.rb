726cb90b6edfac07136c62e293e67555afe3eba1
Valeria Serci <balerty84@gmail.com>
2023-02-15 17:43:13 +0000

    add test when empty string

--- spec/time_tracker_spec.rb
+++ spec/time_tracker_spec.rb
@@ -1,9 +1,11 @@
 require 'time_tracker'
 
 RSpec.describe 'time_tracker' do
-  it 'returns "0 minutes and 1 second" when 5 words' do
-    expect(time_tracker('A sentence with five words')).to eq '0 minutes and 1 second'
+  it 'returns "0 minutes and 0 seconds" when empty string' do
+    expect(time_tracker('')).to eq '0 minutes and 0 seconds'
   end
 
-  
+  #it 'returns "0 minutes and 1 second" when 5 words' do
+   # expect(time_tracker('A sentence with five words')).to eq '0 minutes and 1 second'
+  #end
 end
