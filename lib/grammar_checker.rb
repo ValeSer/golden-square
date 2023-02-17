dbdef291ce4bd9f0eeb162de7ae58cb505f6f118
Valeria Serci <balerty84@gmail.com>
2023-02-17 10:15:59 +0000

    implement right code for modified tests

--- lib/grammar_checker.rb
+++ lib/grammar_checker.rb
@@ -1,5 +1,17 @@
 def grammar_checker(text)
-  raise 'Invalid string' unless text.is_a?(String)
-  text = text.capitalize
-  text == '' ?  text : text + '.'
+  raise "Invalid input" unless text.is_a?(String)
+  return "This is an empty string!" if text == ''
+  t = text.capitalize
+  l = text.chars.last
+  if text == t && l.match?(/[.?!]/)
+    return "Very good! You are the best at grammar!"
+  elsif text == t && text.match?(/[^.?!]/)
+    return "Almost there, you have a capital but are missing punctuation."
+  elsif text != t && l.match?(/[.?!]/)
+    return "Almost there, you have suitable punctuation, but no capital."
+  end
+
+  unless text == t && l.match?(/[.?!]/)
+    return "This has no capital, and no punctuation." 
+  end
 end
