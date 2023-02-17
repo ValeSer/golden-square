02c9b803fa75c516f1cc91310c9d24fdddb3b919
Valeria Serci <balerty84@gmail.com>
2023-02-17 10:13:10 +0000

    modify test file for grammar checker in a better way

--- spec/grammar_checker_spec.rb
+++ spec/grammar_checker_spec.rb
@@ -1,24 +1,28 @@
 require 'grammar_checker'
 
-describe 'grammar_checker' do
-  it 'returns empty string if no argument' do
-    expect(grammar_checker('')).to eq ''
+describe 'grammar_checker method' do
+  it 'returns this is an empty string' do
+    result = grammar_checker('')
+    expect(result).to eq "This is an empty string!"
   end
 
-  it 'returns capitalised text' do
-    expect(grammar_checker('we')).to eq 'We.'
+  it "has no punctuation or capital" do
+    expect(grammar_checker("a new string")).to eq "This has no capital, and no punctuation."
   end
 
-  it 'returns "." at the end of the text' do
-    expect(grammar_checker('we')).to eq 'We.'
+  it "has capital but no punctuation" do
+    expect(grammar_checker("Capital string")).to eq "Almost there, you have a capital but are missing punctuation."
   end
 
-  it 'returns "We are happy to have you here."' do
-    expect(grammar_checker('we are happy to have you here')).to eq 'We are happy to have you here.'
+  it "has a question mark but no Capital" do
+    expect(grammar_checker("this is a question?")).to eq "Almost there, you have suitable punctuation, but no capital."
   end
 
-  it 'raises Invalid string' do
-    expect { grammar_checker(45959) }.to raise_error 'Invalid string'
+  it 'raises error for Invalid input' do
+    expect { grammar_checker(123) }.to raise_error "Invalid input"
   end
 
+  it "text is grammatically correct" do
+    expect(grammar_checker("Correct!")).to eq "Very good! You are the best at grammar!"
+  end
 end
