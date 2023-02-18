7c178c590d82cbbd908c57d4f9f5d1f295a1343c
Valeria Serci <balerty84@gmail.com>
2023-02-18 16:04:02 +0000

    debug ex 3

--- lib/debug_3.rb
+++ lib/debug_3.rb
@@ -1,12 +1,18 @@
 def get_most_common_letter(text)
+  
   counter = Hash.new(0)
   text.chars.each do |char|
-    counter[char] += 1
+    unless char == ' ' then counter[char] += 1 end
   end
-  counter.to_a.sort_by { |k, v| v }[0][0]
+  puts counter
+  p counter.to_a
+  p counter.to_a.sort_by { |k, v| v }
+  return counter.to_a.sort_by { |k, v| v }[-1][0]
+  
+
 end
 
 # Intended output:
 # 
-# > get_most_common_letter("the roof, the roof, the roof is on fire!")
+puts get_most_common_letter("the roof, the roof, the roof is on fire!")
 # => "o"
