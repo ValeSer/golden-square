4efb74af12b5f6c6ed4ed47fd5f6579a647c3a1d
Valeria Serci <balerty84@gmail.com>
2023-02-20 12:16:47 +0000

    test for contents DiaryEntry

--- spec/diary_entry_spec.rb
+++ spec/diary_entry_spec.rb
@@ -4,6 +4,7 @@ describe DiaryEntry do
   it 'constructs' do
     diary_entry = DiaryEntry.new('my_title', 'my_contents')
     expect(diary_entry.title).to eq "my_title"
+    expect(diary_entry.contents).to eq "my_contents"
   end
 
 end
