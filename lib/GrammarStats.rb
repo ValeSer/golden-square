c87eebbc97d1759f25ce9bba21265ab9803cb6a0
Valeria Serci <balerty84@gmail.com>
2023-02-22 13:35:45 +0000

    tests for percentage_good and implement code to pass them

--- lib/GrammarStats.rb
+++ lib/GrammarStats.rb
@@ -1,11 +1,23 @@
 class GrammarStats
   def initialize
+    @total_checked = 0
+    @passed = 0
   end
 
   def check(text)
-    text.match?(/^[A-Z].*[.?!]$/)
+    result = text.match?(/^[A-Z].*[.?!]$/)
+    @total_checked +=1
+    if result then @passed += 1 end
+    return result
   end
 
+  def percentage_good
+    if @total_checked != 0
+      @passed * 100 / @total_checked 
+    else 
+      0
+    end
+  end
 
 
 
--- spec/GrammarStats_spec.rb
+++ spec/GrammarStats_spec.rb
@@ -22,7 +22,52 @@ describe GrammarStats do
       expect(grammar_stats.check('hello?')).to eq false
     end
 
-    
+    it 'returns true with punctuation mark and capitalize' do
+      grammar_stats = GrammarStats.new
+      expect(grammar_stats.check('Hello?')).to eq true
+    end
+
+    describe '#percentage_good' do 
+      it 'returns 0 if no check called' do
+        grammar_stats = GrammarStats.new
+        expect(grammar_stats.percentage_good).to eq 0
+      end
+
+      it 'returns 0 if no check passed' do
+        grammar_stats = GrammarStats.new
+        grammar_stats.check('hello')
+        expect(grammar_stats.percentage_good).to eq 0
+      end
+      it 'returns 100 if 1 check passed' do
+        grammar_stats = GrammarStats.new
+        grammar_stats.check('Hello!')
+        expect(grammar_stats.percentage_good).to eq 100
+      end
+      it 'returns 100 if all check passed' do
+        grammar_stats = GrammarStats.new
+        grammar_stats.check('Hello!')
+        grammar_stats.check('Hello!')
+        grammar_stats.check('Hello!')
+        expect(grammar_stats.percentage_good).to eq 100
+      end
+
+      it 'returns 0 if none check passed' do
+        grammar_stats = GrammarStats.new
+        grammar_stats.check('hello')
+        grammar_stats.check('hello')
+        grammar_stats.check('hello')
+        expect(grammar_stats.percentage_good).to eq 0
+      end
+
+      it 'returns 33 if 1/3 passed' do
+        grammar_stats = GrammarStats.new
+        grammar_stats.check('Hello!')
+        grammar_stats.check('hello!')
+        grammar_stats.check('Hello')
+        expect(grammar_stats.percentage_good).to eq 33
+      end
+    end
+
     
   end
 end
